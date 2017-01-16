fda0f553b6155ada568d671f93fb70603cb937d7
connorwalsh <c@polygon.pizza>
2017-01-16 11:18:50 -0500

    [cw|1.16.2017] Added onmessage handler for ws.

--- src/socketEvents.js
+++ src/socketEvents.js
@@ -2,10 +2,17 @@ import io from 'socket.io-client'
 
 const handleWebSocketEvents = () => {
   // TODO (cw|1.15.2017) change hard-coded address to programmatic
-  let ws = new WebSocket('ws://127.0.0.1:5555/ws')
+  let ws = new WebSocket('ws://127.0.0.1:3145/ws')
 
+  // on websocket open
   ws.onopen = (event) => {
+    ws.send('sup')
     console.log('OPENED!')
+  }
+
+  // on incoming message
+  ws.onmessage = ({data}) => {
+    console.log(data)
   }
 }
 
