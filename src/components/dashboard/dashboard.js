a0bdf6fa2ba6f5635d720174d803118c1c15ae20
connorwalsh <c@polygon.pizza>
2017-01-25 02:03:27 -0500

    [cw|1.25.2017] Added menus to dashboard.

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
+import IconButton from 'material-ui/IconButton'
+import Menu from 'material-ui/svg-icons/navigation/Menu'
+import Share from 'material-ui/svg-icons/social/Share'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import './dashboard.css'
 
+// we need this for this component to work with AppBar
+injectTapEventPlugin()
+
 const Dashboard = (props) => {
+  const iconStyles = {
+    width: 40,
+    height: 40,
+    color: '#ff9694'
+  }
   return(
-    <div className='dashboard'>
-      {props.session.id}
+      <div className='dashboard'>
+        <IconMenu
+          iconButtonElement={
+            <IconButton iconStyle={iconStyles}>
+              <Menu/>
+            </IconButton>
+          }
+    anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+    targetOrigin={{horizontal: 'left', vertical: 'top'}}
+      />
+        <IconMenu
+          iconButtonElement={
+            <IconButton iconStyle={iconStyles}>
+              <Share/>
+            </IconButton>
+          }
+    anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+    targetOrigin={{horizontal: 'left', vertical: 'top'}}
+      >
+      <MenuItem primaryText={props.session.id}/>
+        </IconMenu>
     </div>
   )
 }
