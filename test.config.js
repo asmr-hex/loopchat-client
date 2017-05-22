1dcd80d69edbd91522bd4b261995bad0845699dd
connorwalsh <c@polygon.pizza>
2017-05-22 14:13:30 -0400

    [cw|#3,#7] add opts argument to mocha-webpack. It seems that mocha-webpack couldn't find this on travis.

--- test.config.js
+++ test.config.js
@@ -1,8 +1,10 @@
 var prodConfig = require('./webpack.config')
+var nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   resolve: prodConfig.resolve,
   target: 'node',
+  externals: [nodeExternals()],
   devtool: 'source-map',
   output: { // for sourcemap support
     devtoolModuleFilenameTemplate: '[absolute-resource-path]',
