b5254a7f59b4947955a0354a25829c925bcbeed8
connorwalsh <c@polygon.pizza>
2017-11-03 13:20:21 -0400

    update to react16 and introduce enzyme test setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var webpack = require('webpack');
 
+// TODO (cw|11.3.2017) migrate from webpack 1.x.x -> 2.0.0
+
 module.exports = {
   devtool: 'inline-source-map',
   resolve: {
@@ -30,6 +32,10 @@ module.exports = {
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
       },
     ]
   }
