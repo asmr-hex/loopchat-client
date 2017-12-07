e7c57375d10c89aab22d17936b5ab788f919f565
connorwalsh <c@polygon.pizza>
2017-12-07 12:46:07 -0500

    add timeline input transitions for expansion and collapsing

--- src/components/editor/panels/input/control.js
+++ src/components/editor/panels/input/control.js
@@ -6,11 +6,22 @@ import {IconButton, IconMenu, MenuItem} from 'material-ui'
 import AddIcon from 'material-ui/svg-icons/content/add'
 import MidiIcon from 'material-ui/svg-icons/action/settings-input-svideo'
 import AudioIcon from 'material-ui/svg-icons/image/music-note'
+import CollapseIcon from 'material-ui/svg-icons/navigation/chevron-left'
+import ExpandIcon from 'material-ui/svg-icons/navigation/chevron-right'
 import {getTracksFromTimeline} from '../../../../redux/selectors/timelines'
 import {addNewTrackToTimeline} from '../../../../redux/actions/timelines/timelines'
 import {TrackInput} from './trackInput'
 import styles from './index.css'
-import {purple, green, grey, lightGrey, yellow, red} from  '../../../../styles/palette.css'
+import {
+  blue,
+  purple,
+  green,
+  grey,
+  lightGrey,
+  midGrey,
+  yellow,
+  red,
+} from  '../../../../styles/palette.css'
 
 
 const actions = {
@@ -27,6 +38,12 @@ export class InputControlPanel extends Component {
     timelineId: string.isRequired,
   }
 
+  constructor(props) {
+    super(props)
+
+    this.state = {collapsed: false}
+  }
+  
   renderTrackInputs() {
     const {tracks} = this.props
 
@@ -50,44 +67,58 @@ export class InputControlPanel extends Component {
     const svg = document.getElementById(iconId)
     svg.style.fill = color
   }
+
+  expandCollapse() {
+    this.setState({collapsed: !this.state.collapsed})
+  }
   
   render() {
+    const {collapsed} = this.state
+
+    const panelStyle = {
+      collapsed: {flexBasis: '0%'},
+      expanded: {flexBasis: '20%'},
+    }
+    
     const addIcon = (
-      <IconButton>
+      <IconButton tooltip="add track">
         <AddIcon color={purple} hoverColor={green}/>
       </IconButton>
     )
-
+    
     const midiIconId = 'midi-icn'
     const audioIconId = 'audio-icn'
-    const midiIcon = (<MidiIcon id={midiIconId} color={yellow} hoverColor={red}/>)
-    const audioIcon = (<AudioIcon id={audioIconId} color={yellow} hoverColor={red}/>)
+    const midiIcon = (<MidiIcon id={midiIconId}/>)
+    const audioIcon = (<AudioIcon id={audioIconId}/>)
     const menuItemStyles = {color: lightGrey}
     
     return (
-      <div className={styles.inputControlPanel}>
+      <div className={styles.inputControlPanel} style={collapsed ? panelStyle.collapsed : panelStyle.expanded }>
         <div className={styles.inputControlMenu}>
           <IconMenu
             iconButtonElement={addIcon}
             anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
             menuStyle={{backgroundColor: grey}}
             >
-            <MenuItem
-              onClick={() => this.addTrack()}
-              primaryText="MIDI"
-              leftIcon={midiIcon}
-              style={menuItemStyles}
-              onMouseEnter={(e) => this.updateItemColors(e, midiIconId, red)}
-              onMouseLeave={(e) => this.updateItemColors(e, midiIconId, yellow)}
-              />
-            <MenuItem
-              primaryText="Audio"
-              leftIcon={audioIcon}
-              style={menuItemStyles}
-              onMouseEnter={(e) => this.updateItemColors(e, audioIconId, red)}
-              onMouseLeave={(e) => this.updateItemColors(e, audioIconId, yellow)}
-              />
+          <MenuItem
+            onClick={() => this.addTrack()}
+            primaryText="MIDI"
+            leftIcon={midiIcon}
+            style={menuItemStyles}
+            onMouseEnter={(e) => this.updateItemColors(e, midiIconId, red)}
+            onMouseLeave={(e) => this.updateItemColors(e, midiIconId, yellow)}
+            />
+          <MenuItem
+            primaryText="Audio"
+            leftIcon={audioIcon}
+            style={menuItemStyles}
+            onMouseEnter={(e) => this.updateItemColors(e, audioIconId, red)}
+            onMouseLeave={(e) => this.updateItemColors(e, audioIconId, yellow)}
+            />
           </IconMenu>
+          <IconButton tooltip={collapsed ? 'expand' : 'collapse'} onClick={() => this.expandCollapse()}>
+            {collapsed ? <ExpandIcon color={blue} hoverColor={green}/> : <CollapseIcon color={blue} hoverColor={green}/>}
+          </IconButton>
         </div>
         <div className={styles.trackInputPanelContainer}>
           {this.renderTrackInputs()}
--- src/styles/muiTheme.js
+++ src/styles/muiTheme.js
@@ -1,9 +1,11 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import {black, white} from './palette.css'
 
 
 // TODO (cw|11.25.2017) put all common styles in here...
 export const wwvvMuiTheme = getMuiTheme({
   fontFamily: 'Share Techno Mono, monospace',
   palette: {canvasColor: '#00000000'},
+  tooltip: {rippleBackgroundColor: black, color: white},
 })
 
